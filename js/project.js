dca2834a8daee8ce54ad4217cbbbbe67d776cbdd
navjot <navjotkaurgill05@gmail.com>
2021-04-04 16:12:35 -0400

    portfolio

---
+++ js/project.js
@@ -0,0 +1,15 @@
+function displayDescriptionCovid() {
+	document.getElementById("proj-desc-covid").style.display = "block";
+	document.getElementById("proj-desc-paradise").style.display = "none";
+	document.getElementById("proj-desc-overthemoon").style.display = "none";
+}
+function displayDescriptionParadise() {
+	document.getElementById("proj-desc-covid").style.display = "none";
+	document.getElementById("proj-desc-paradise").style.display = "block";
+	document.getElementById("proj-desc-overthemoon").style.display = "none";
+}
+function displayDescriptionOverTheMoon() {
+	document.getElementById("proj-desc-covid").style.display = "none";
+	document.getElementById("proj-desc-paradise").style.display = "none";
+	document.getElementById("proj-desc-overthemoon").style.display = "block";
+}
